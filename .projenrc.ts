0faf279856d7705700f30148baddd7b3ccbdc3a8
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2025-12-17 07:50:33 +0100

    chore(projen): repository configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,144 @@
-import { awscdk } from 'projen';
+import { awscdk, ReleasableCommits } from 'projen';
+import { GithubCredentials } from 'projen/lib/github';
+import { AppPermission } from 'projen/lib/github/workflows-model';
+import {
+  TrailingComma,
+  UpgradeDependenciesSchedule,
+} from 'projen/lib/javascript';
+import { ReleaseTrigger } from 'projen/lib/release';
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Antonio Márquez Pérez',
+  author: 'JumpToTheCloud',
   authorAddress: 'antonio.marquez@jumptothecloud.tech',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.232.2',
+  constructsVersion: '10.4.2',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.9.0',
-  name: 'aws-ipam',
+  name: '@jttc/aws-ipam',
   projenrcTs: true,
   repositoryUrl: 'git@github.com:JumpToTheCloud/aws-ipam.git',
-
+  keywords: [
+    'aws',
+    'cdk',
+    'ipam',
+    'ipam pool',
+    'ipam scope',
+    'ipam allocation',
+  ],
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      trailingComma: TrailingComma.ES5,
+      singleQuote: true,
+      bracketSpacing: true,
+      semi: true,
+    },
+  },
+  autoMerge: false,
+  mergify: false,
+  release: true,
+  releaseTrigger: ReleaseTrigger.workflowDispatch(),
+  releasableCommits: ReleasableCommits.featuresAndFixes(),
+  depsUpgrade: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  majorVersion: 1,
+  prerelease: 'beta',
+  jestOptions: {
+    jestConfig: {
+      verbose: true,
+    },
+  },
+  githubOptions: {
+    projenCredentials: GithubCredentials.fromApp({
+      permissions: {
+        pullRequests: AppPermission.WRITE,
+        contents: AppPermission.WRITE,
+      },
+    }),
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: [
+          'feat',
+          'fix',
+          'chore',
+          'docs',
+          'style',
+          'refactor',
+          'test',
+          'revert',
+          'ci',
+        ],
+      },
+    },
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  devDeps: [
+    'commitizen',
+    'cz-customizable',
+    'jest-runner-groups',
+    'jest-runner',
+    'jest-docblock',
+  ],
+  packageName: '@jttc/aws-ipam',
 });
+
+project.addTask('commit', {
+  description:
+    'Commit changes with conventional commits prompts provided by Commitizen',
+  steps: [
+    {
+      exec: './node_modules/cz-customizable/standalone.js',
+      receiveArgs: false,
+      say: 'committing changes',
+    },
+  ],
+});
+
+const unitTest = project.addTask('test:unit', {
+  description: 'Unit Tests',
+  steps: [
+    {
+      exec: 'jest --group=unit',
+      say: 'Unit Tests',
+      receiveArgs: true,
+    },
+  ],
+});
+
+project.addTask('test:snapshot', {
+  description: 'Snapshots Tests',
+  steps: [
+    {
+      exec: 'jest --group=snapshot --collectCoverage=false',
+      say: 'Snapshots Tests',
+      receiveArgs: true,
+    },
+  ],
+});
+
+project.addTask('test:snapshot:update', {
+  description: 'Update snapshots',
+  steps: [
+    {
+      exec: 'jest --updateSnapshot --collectCoverage=false',
+      say: 'Updating snapshots',
+      receiveArgs: true,
+    },
+  ],
+});
+
+const testTask = project.tasks.tryFind('test');
+const eslintTask = project.tasks.tryFind('eslint');
+
+if (testTask && eslintTask) {
+  testTask.reset();
+  testTask.spawn(unitTest);
+  testTask.spawn(eslintTask);
+}
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@jttc/aws-ipam.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@jttc/aws-ipam.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@jttc/aws-ipam'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@jttc/aws-ipam.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@jttc/aws-ipam.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
